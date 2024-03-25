5a85e4a834b198dbb67bafb57cc9909a4b47024b
Gabrihelln <bihellaraujo@gmail.com>
2024-03-25 14:33:09 -0300

    menucategories

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import styles from "./menu.module.css"
+import Link from "next/link";
+import Image from "next/image";
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories"
 
 const Menu = () => {
     return (
-        <div className= {styles.container}>Menu</div>
+        <div className= {styles.container}>
+            <h2 className={styles.subtitle}>{"Tendencias"}</h2>
+            <h1 className={styles.title}>Mais Populares</h1>
+            <MenuPosts withImage={false}/>
+            <h2 className={styles.subtitle}>Navegue pelos Tópicos</h2>
+            <h1 className={styles.title}>Categorias</h1>
+            <MenuCategories/>
+            <h2 className={styles.subtitle}>Escolha dos Editores</h2>
+            <h1 className={styles.title}>Editores</h1>
+           <MenuPosts withImage={true}/>
+        </div>
     );
 };
 
