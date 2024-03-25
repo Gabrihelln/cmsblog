207df38c455083db1a1d8957700ed3321fed077c
Gabrihelln <bihellaraujo@gmail.com>
2024-03-25 19:11:22 -0300

    button pagination

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -3,7 +3,10 @@ import styles from "./pagination.module.css"
 
 const Pagination = () => {
     return (
-        <div className= {styles.container}>Pagination</div>
+        <div className= {styles.container}>
+            <button className={styles.button}>Anterior</button>
+            <button className={styles.button}>Próximo</button>
+        </div>
     );
 };
 
