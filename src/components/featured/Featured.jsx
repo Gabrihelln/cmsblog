96be03a84a38cd653bbaa8938e0dbe9d4cd44a75
Gabrihelln <bihellaraujo@gmail.com>
2024-03-25 11:42:08 -0300

    component featured

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import styles from "./featured.module.css"
+import Image from "next/image";
 
 const Featured = () => {
     return (
-        <div className= {styles.container}>Featured</div>
+        <div className= {styles.container}>
+            <h1 className={styles.title}>
+                <b>Lorem, ipsum dolor sit!</b> Amet consectetur adipisicing elit.</h1>
+            <div className={styles.post}>
+                <div className={styles.imgContainer}>
+                    <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
+                </div>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                    <p className={styles.postDesc}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat ratione at impedit!</p>
+                    <button className={styles.button}>Leia Mais</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
