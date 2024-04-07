ee0aebb63b34eda8b941b5d9d9d5b4627f3fe596
Gabrihelln <bihellaraujo@gmail.com>
2024-04-07 20:13:21 -0300

    prisma check

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,40 +1,32 @@
-"use client"
-
+"use client";
 import { signIn, useSession } from "next-auth/react";
-import styles from "./loginPage.module.css"
+import styles from "./loginPage.module.css";
 import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const { status } = useSession();
 
-    const {data,status} = useSession()
+  const router = useRouter();
 
-    const router = useRouter()
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
 
-   if (status === "loading") {
-    return 
-        <div className={styles.loading}>Loading...</div>
-   }
-
-   if (status === "authenticated") {
+  if (status === "authenticated") {
     router.push("/")
-       
-   }
-    
-    return (
-        <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <div 
-                    className={styles.socialButton} onClick={()=>signIn("google")}>Entre com o do Google
-                </div>
-                <div 
-                    className={styles.socialButton}>Entre com o Facebook
-                </div>
-                <div 
-                    className={styles.socialButton}>Entre com o GitHub
-                </div>
-            </div>
+  }
+  
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.socialButton} onClick={() => signIn("google")}>
+          Sign in with Google
         </div>
-    )
-}
+        <div className={styles.socialButton}>Sign in with Github</div>
+        <div className={styles.socialButton}>Sign in with Facebook</div>
+      </div>
+    </div>
+  );
+};
 
 export default LoginPage;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,11 @@
 "use client"
 
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 
 const AuthProvider = ({children}) => {
-    return (
-        <SessionProvider>{children}</SessionProvider>
-    )
+  return (
+    <SessionProvider>{children}</SessionProvider>
+  )
 }
 
-export default AuthProvider;
+export default AuthProvider
